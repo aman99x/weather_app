5338419cf9a3b0a200fc67f87f7c1a19740dc3e1
aman <aman999060@gmail.com>
2023-12-23 00:22:23 +0530

    update file

--- routes/fetchWeather.js
+++ routes/fetchWeather.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const fetchWeatherData = async (location) => {
     const apiKey = '41cfce50acfcb1eefa0e78ccf7a6e9d4';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
     try {
       const response = await axios.get(apiUrl);
       const { name, main, weather } = response.data;
